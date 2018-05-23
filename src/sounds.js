9f134204347df746aff8b23d66267048389a028d
Ammar L <ammarlalji212@gmail.com>
2018-05-23 12:39:07 -0400

    First Playable
    uploading our our work so far

---
+++ src/sounds.js
@@ -0,0 +1,9 @@
+var JumpSFX= new Audio('../sounds/jump.wav');
+var BackgroundSFX = new Audio('../sounds/background1.wav');
+var BackgroundSFX1= new Audio('../sounds/background2.wav');
+var BackgroundSFX2= new Audio('../sounds/background3.wav');
+var BackgroundSFX3= new Audio('../sounds/background3.wav');             //More SFX will be added here//
+var DeathSFX= new Audio('../sounds/die.wav');
+var EnemyDeathSFX= new Audio('../sounds/enemydie.wav');
+var VictorySFX= new Audio('../sounds/victory1.wav');
+
